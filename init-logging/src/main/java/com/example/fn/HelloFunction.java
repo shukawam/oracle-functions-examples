b4bafc45526bf1cda995f5f95847821f0e633b6c
shukawam <shukawam@gmail.com>
2022-03-10 10:22:32 +0900

    Added constructor

--- init-logging/src/main/java/com/example/fn/HelloFunction.java
+++ init-logging/src/main/java/com/example/fn/HelloFunction.java
@@ -1,6 +1,18 @@
 package com.example.fn;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Logger;
+
 public class HelloFunction {
+    private static final Logger LOGGER = Logger.getLogger(HelloFunction.class.getName());
+
+    public HelloFunction() {
+        Date now = new Date();
+        SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
+        LOGGER.info(sf.format(now));
+        LOGGER.info("HelloFunction constructor");
+    }
 
     public String handleRequest(String input) {
         String name = (input == null || input.isEmpty()) ? "world"  : input;
