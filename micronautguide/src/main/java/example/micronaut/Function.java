9a94c9d475449f6b20fe34cd1860b041de3971de
shukawam <shukawam@gmail.com>
2022-03-11 06:52:21 +0000

    Initial commit

---
+++ micronautguide/src/main/java/example/micronaut/Function.java
@@ -0,0 +1,19 @@
+package example.micronaut;
+
+import io.micronaut.core.annotation.ReflectiveAccess;
+import io.micronaut.oraclecloud.function.OciFunction;
+import io.micronaut.oraclecloud.core.TenancyIdProvider;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+
+@Singleton
+public class Function extends OciFunction {
+    @Inject
+    TenancyIdProvider tenantIdProvider;
+
+    @ReflectiveAccess
+    public String handleRequest() {
+        String tenancyId = tenantIdProvider.getTenancyId();
+        return "Your tenancy is: " + tenancyId;
+    }
+}
